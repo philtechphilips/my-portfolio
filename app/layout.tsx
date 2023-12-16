519fbfc0db20f772d5107bb7517b3aa8bf31e70f
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2023-12-16 01:56:48 +0100

    feat: Homepage ongoing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import 'gsap/dist/gsap';
+import 'gsap/dist/ScrollTrigger';
+
+// Your component code here
+
 
 const inter = Inter({ subsets: ['latin'] })
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import gsap, { Expo } from 'gsap';
 
 const Navbar = () => {
+  useEffect(() => {
+    const title = document.getElementById('title');
+    const menu = document.getElementById('menu');
+
+    gsap.set([title, menu], { opacity: 0, y: 20 });
+
+    const fadeUp = gsap.timeline({ defaults: { ease: 'power2.out' } })
+      .to([title, menu], { opacity: 1, y: 0, duration: 1.5, ease: Expo.easeInOut });
+
+    return () => {
+      fadeUp.kill();
+    };
+  }, []);
   return (
-    <div className='bg-black w-full px-5 md:px-10 py-7 flex justify-between items-center fixed top-0 left-0'>
-      <h1 className='text-white font-medium text-[18px]'>Isola Pelumi</h1>
-      <h1 className='text-white text-[16px] cursor-pointer'>MENU +</h1>
+    <div id='background' className='bg-black w-full px-5 md:px-10 py-7 flex justify-between items-center fixed top-0 left-0'>
+     <h1 id="title" className='text-white font-medium text-[18px]'>Isola Pelumi</h1>
+      <h1 id="menu" className='text-white text-[16px] cursor-pointer'>MENU +</h1>
     </div>
   )
 }
