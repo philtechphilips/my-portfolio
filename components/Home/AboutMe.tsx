b3fcc6701d575f7f72950eff1d930c5c4191b627
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2023-12-16 21:37:40 +0100

    refactor

--- components/Home/AboutMe.tsx
+++ components/Home/AboutMe.tsx
@@ -7,61 +7,71 @@ import 'remixicon/fonts/remixicon.css';
 const AboutMe = () => {
     return (
         <>
-            <div className="bg-black w-full px-5 md:px-10 h-[600px] md:h-screen flex gap-0 flex-col justify-center">
-                <i className="ri-arrow-right-down-line text-[#A3A3A3] text-xl md:text-9xl"></i>
-                <div className='w-fit'>
-                    <h1 className='text-[#A3A3A3] text-[60px] md:text-[130px] font-semibold md:font-medium '>SOFTWARE</h1>
-                    <h1 className='text-[#A3A3A3] text-[60px] md:text-[130px] font-semibold md:font-medium md:ml-40 md:-mt-20'>ENGINEER</h1>
-                    <p className='text-white text-right md:-mt-8 font-semibold'>BASED IN LAGOS</p>
+            <div className="w-full px-5  md:h-screen flex gap-0 flex-col md:-mt-4 justify-center">
+                <div className='bg-black w-full h-[.5px]'></div>
+                <div className='w-full'>
+                    <h1 className='text-black text-center text-[60px] md:text-[180px] font-semibold md:font-medium '>SOFTWARE</h1>
+                    <div className='bg-black w-full h-[.5px]'></div>
+                    <div className='flex items-center justify-between'>
+                    <Image src="/images/asterisk.svg" alt='asterick' width={15} height={15} className='md:hidden' />
+                        <Image src="/images/asterisk.svg" alt='asterick' width={50} height={50} className='hidden md:block' />
+                        <h1 className='text-black text-center text-[60px] md:text-[180px] font-semibold md:font-medium'>
+                            ENGINEER
+                        </h1>
+                        <Image src="/images/asterisk.svg" alt='asterick' width={50} height={50} className='hidden md:block' />
+                        <Image src="/images/asterisk.svg" alt='asterick' width={15} height={15} className='md:hidden' />
+                    </div>
+                    <div className='bg-black w-full h-[.5px]'></div>
                 </div>
             </div>
 
-            <div className='w-full bg-black px-5 md:px-10 md:pt-20'>
-                <div className='md:border-t  border-t-[#A3A3A3] w-full pt-10 py-10 md:py-0'>
+            <div className='w-full px-5'>
+                <div className='w-full pt-10 py-10 md:py-0'>
                     <div className='flex flex-col md:flex-row justify-between md:py-20'>
                         <div className='w-1/3'>
-                            <h5 className='text-white uppercase text-lg'>/About Me</h5>
+                            <h5 className='text-black uppercase text-lg font-[Gt]'>/About Me</h5>
                         </div>
                         <div className='w-full md:w-2/3 flex flex-col gap-7'>
-                            <p className='uppercase text-white md:text-4xl leading-8 md:leading-[50px] font-sans'>
+                            <p className='uppercase text-black md:text-4xl leading-8 md:leading-[50px] font-sans'>
                                 a passionate software engineer with 3 years of hands-on experience.
                                 My skills span PHP, Laravel, JavaScript, TypeScript, React.js, Node.js, and more. I love turning ideas into reality by seamlessly merging front-end and back-end engineering solutions.
                             </p>
                             <Image src="/images/picture.png" alt='my Image' width="300" height={400} />
-                            <p className='uppercase text-white  md:text-4xl leading-8 md:leading-[50px] font-sans'>
+                            <p className='uppercase text-black  md:text-4xl leading-8 md:leading-[50px] font-sans'>
                                 I've tackled exciting projects, including enhancing React, NodeJs and Express applications.
                             </p>
 
-                            <p className='uppercase text-white  md:text-4xl leading-8 md:leading-[50px] font-sans'>
+                            <p className='uppercase text-black  md:text-4xl leading-8 md:leading-[50px] font-sans'>
                                 I'm committed to continuous learning, collaboration, and innovation, aiming to specialize in emerging technologies for impactful solutions.
                             </p>
                         </div>
                     </div>
                 </div>
+                <div className='bg-black w-full h-[.5px]'></div>
             </div>
 
-            <div className='w-full bg-black px-5 md:px-10 md:py-20'>
-                <div className='md:border-t border-t-[#A3A3A3] md:border-b border-b-[#A3A3A3] w-full'>
-                    <div className='flex flex-col md:flex-row justify-between py-20'>
+            <div className='w-full px-5'>
+                <div className='w-full'>
+                    <div className='flex flex-col md:flex-row justify-between py-10'>
                         <div className='w-1/3'>
-                            <h5 className='text-white uppercase text-lg'>/Skillset</h5>
+                            <h5 className='text-black uppercase text-lg font-[Gt]'>/Skillset</h5>
                         </div>
                         <div className='w-full md:w-2/3 mt-0'>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-5 md:text-[80px] font-semibold uppercase md:mt-16'>CREATIVE</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>Teamplayer</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>Critical thinker</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>html</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>css</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>bootstrap</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>tailwind css</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>mui</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>scss</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>sql/mysql</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>Mongodb</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>redis</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>react/next</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>nodejs/express</h1>
-                            <h1 className='text-[#A3A3A3] text-2xl mt-3 md:text-[80px] font-semibold uppercase md:mt-16'>php/laravel</h1>
+                            <h1 className='text-center text-4xl mt-5 md:text-[80px] capitalize md:mt-16'>Creative</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>Teamplayer</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>Critical thinker</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>html</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>css</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>bootstrap</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>tailwind css</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>mui</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>scss</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>sql/mysql</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>Mongodb</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>redis</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>react/next Js</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>nodejs/express</h1>
+                            <h1 className='text-center text-4xl mt-3 md:text-[80px] capitalize md:mt-16'>php/laravel</h1>
                         </div>
                     </div>
                 </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import gsap, { Expo } from 'gsap';
 
 const Navbar = () => {
+  const [isMenuHovered, setIsMenuHovered] = useState(false);
+
   useEffect(() => {
     const title = document.getElementById('title');
     const menu = document.getElementById('menu');
@@ -16,10 +18,18 @@ const Navbar = () => {
       fadeUp.kill();
     };
   }, []);
+
+
   return (
-    <div id='background' className='bg-black w-full px-5 md:px-10 py-7 flex justify-between items-center fixed top-0 left-0'>
-     <h1 id="title" className='text-white font-medium text-[18px]'>Isola Pelumi</h1>
-      <h1 id="menu" className='text-white text-[16px] cursor-pointer'>MENU +</h1>
+    <div className='w-full px-5 py-7 flex justify-between items-center'>
+      <div className='hidden md:flex gap-5 justify-center '>
+      <h1 id="title" className='text-black text-[12px] font-[100] font-[GT]'>Gh.</h1>
+      <h1 id="title" className='text-black text-[12px] font-[100] font-[GT]'>Ln.</h1>
+      <h1 id="title" className='text-black text-[12px] font-[100] font-[GT]'>In.</h1>
+      <h1 id="title" className='text-black text-[12px] font-[100] font-[GT]'>Tw.</h1>
+      </div>
+      <h1 id="title" className='text-black text-[12px] font-[100] font-[GT] uppercase tracking-[0.18em]'>I s o l a &nbsp;P e l u m i</h1>
+      <h1 id="title" className='text-black text-[12px] font-[100] font-[GT] uppercase tracking-[0.18em]'>21:00PM</h1>
     </div>
   )
 }
